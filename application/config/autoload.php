e1884da4085219441b388c8f41e40410ed336326
Daniel Tian <daniel_tian2002@hotmail.com>
2017-01-27 17:50:43 -0800

    added database support for site, changing from a static site to a dynamic web app

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -50,15 +50,14 @@ $autoload['packages'] = array();
 | 'database' library, which is somewhat of a special case.
 |
 | Prototype:
-|
 |	$autoload['libraries'] = array('database', 'email', 'session');
-|
+|$autoload['libraries'] = array('parser');
 | You can also supply an alternative library name to be assigned
 | in the controller:
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser');
+$autoload['libraries'] = array('database', 'parser');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +131,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('images');
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,29 @@ class Welcome extends Application
 	public function index()
 	{
 
+		$this->load->model('Images');
+		
+		//get all the images from our model
+		$pix = $this->Images->newest();
+
+		//build array of formatted cells for them
+		foreach($pix as $picture)	
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+
+		//prime the table class
+		$this->load->library('table');
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td calss="oneimage">'
+		);
+		$this->table->set_template($parms);
+
+		//generate the table
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+
+
 		$this->data['pagebody'] = 'welcome';
 		$this->render();
 	}
